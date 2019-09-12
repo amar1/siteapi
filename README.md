6250b9d76b0a1400adc2f4fa1710f382505dab69
Amrendra Mourya <amrendra@qs.com>
2019-09-12 21:55:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# siteapi module
+
+## Features
+
+1. As an administrative user of a Drupal 8 site.
+You can setup a site api key in admin site information page.
+
+2. As Anonymous or Authenticated user you can access `page` content in JSON
+format on your site if you have the below details:
+
+ - Site API Key
+ - Node ID of the content.
+
+## How to install Site API Module?
+
+1. Download siteapi module from github.
+2. Copy in modules folder.
+3. Enable the siteapi module using via drush command `drush en siteapi`
+
+## How to configure site api key?
+
+1. Login as a administrator,
+2. Goto `/admin/config/system/site-information`
+4. Enter the Site API key for Example : `FOOBAR12345` and Save the Form.
+5. Create page type content in your drupal 8 site.
+6. Go to the link of the format `http://localhost/page_json/{api_key}/{nid}`
+
+  Example: `http://localhost/page_json/FOOBAR12345/2`
+
+## Return JSON Output
+
+1. If API Key is correct and Node ID of a `Page` content.
+
+`{
+  "Node ID": "1",
+  "title": "Page type content",
+  "body": [
+    {
+      "value": "<p>Lorem Lipsum</p>\r\n",
+      "summary": "",
+      "format": "basic_html"
+    }
+  ],
+  "Node Type": null,
+  "Node Access": "Success"
+}`
+
+2. If API key is wrong.
+
+`[
+  "Access Denied",
+  "Reason : API Key Invalid"
+]`
+
+
+
+### References
+
+- [Drupal 8 Form API](https://www.drupal.org/docs/8/api/form-api/introduction-to-form-api)
+- [Drupal 8 Configuration API](https://www.drupal.org/docs/8/api/configuration-api/configuration-api-overview)
+- [Drupal 8 Configuration API to set value](https://www.drupal.org/docs/8/api/configuration-api/simple-configuration-api)
+- [Drupal 8 Form Id alter](https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Form%21form.api.php/function/hook_form_FORM_ID_alter/8.4.x)
+- [Drupal 8 JsonResponse](https://drupal-up.com/blog/custom-drupal-8-module-json-response)
+- [Drupal 8 comment standards](https://www.drupal.org/docs/develop/standards/api-documentation-and-comment-standards)
+
+Total Coding Time - 3 Hours
---
+++ src/Controller/JsonPageNodeController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\siteapi\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Implements JsonPageNodeController for the siteapi module.
+ */
+class JsonPageNodeController extends ControllerBase {
+
+  /**
+   * Function to render JSON output for page node type.
+   *
+   * @param string $api_key
+   *   A String passed from the request URL.
+   * @param int $nid
+   *   A integer passed from the request URL.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *
+   *   A Json Response return Node detail or error Message.
+   */
+  public function renderJson($api_key, $nid) {
+
+    // Get the Site API key from configuration.
+    $site_api_key = \Drupal::config('siteapi.settings')->get('siteapikey');
+
+    // Check if the API Key entered in the URL is Valid.
+    if ($api_key === $site_api_key) {
+
+      // Check if the node id is numeric and greater then 0.
+      if (is_numeric($nid) && $nid > 0) {
+
+        $node = \Drupal::entityTypeManager()->getStorage('node')->load($nid);
+
+        // Check if the node is loaded and it is of type 'page'.
+        if (!empty($node) && $node->getType() === 'page') {
+
+          // Get node title, body, type.
+          $title = $node->getTitle();
+          $body = $node->get('body')->getValue();
+          $type = $node->getType();
+
+          // Build the JSON response.
+          $json_response = [
+            'Node ID' => $nid,
+            'title' => $title,
+            'body' => $body,
+            'Node Type' => $node_type,
+            'Node Access' => 'Success',
+          ];
+
+          // Return the JSON Response.
+          return new JsonResponse($json_response);
+        }
+        else {
+          // If content type page or numerice value is not there.
+          // then JSON response.
+          $json_response = [
+            'Access Denied',
+            'Reason : Node is not existing or not content type of `page`',
+          ];
+          // Return the JSON Response.
+          return new JsonResponse($json_response);
+        }
+
+      }
+
+      // Generate appropriate Response as Node ID is not Numeric.
+      else {
+        // Build appropriate JSON response.
+        $json_response = [
+          'Access Denied',
+          'Reason : Invalid Node ID. Please enter numeric Node ID value only',
+        ];
+        // Return the JSON Response.
+        return new JsonResponse($json_response);
+
+      }
+
+    }
+
+    // Generate appropriate Response as API Key is not Valid.
+    else {
+      // Build appropriate JSON response.
+      $json_response = [
+        'Access Denied',
+        'Reason : API Key Invalid',
+      ];
+      // Return the JSON Response.
+      return new JsonResponse($json_response);
+    }
+
+  }
+
+}
